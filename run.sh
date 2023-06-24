0a335c22fc9b57651ea031644dc1cf546a228a76
Prince Gupta <21282111+prince776@users.noreply.github.com>
2023-06-24 17:23:52 +0530

    SYN ACK working now (testing on ubunut now) (#1)
    * debugging support
    * Remove pcap++
    * set c++20, why was it 23
    * script changes

--- run.sh
+++ run.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-sudo ./build/netstack &
-pid=$!
-echo "pid: ${pid}"
-sleep 3
-sudo ifconfig tun0 inet 10.0.0.1 10.0.0.2 up
-echo "Added ifconfig"
-trap "sudo kill $pid" INT TERM
-wait $pid
+sudo ./build/netstack
